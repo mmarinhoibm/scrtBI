64addc1f62b176aa876fc1f75a102e149a4a5ed6
Marcelo M Marinho <mmarinho@mmarinho-linux>
2017-05-27 21:53:18 -0300

    initialize

---
+++ SCRTCSVServer/app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('string-format');
+
+var port = (process.env.PORT || process.env.VCAP_APP_PORT || 8888);
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World!\n');
+}).listen(port);
+
+console.log('Server running at http://127.0.0.1:'+port);
+	
